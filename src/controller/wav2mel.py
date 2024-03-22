27dc2b5d7eb3d45feae3d1376f8eb3874c52b2d3
mike dlulane <mikedlulane@outlook.com>
2024-03-22 05:47:53 +0200

    Sequential Solution to Convert Audio to Mel Spectrogram.

--- src/controller/wav2mel.py
+++ src/controller/wav2mel.py
@@ -2,34 +2,37 @@ import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt_waveform
+import matplotlib.pyplot as plt_spectrogram
 import matplotlib.style as ms
+import librosa.display
+import librosa.feature
 #ms.use('seaborn-muted')
-
+import os
+from pathlib import Path
 import librosa
 import librosa.display
 import IPython.display
-
-#base_path = "../../data/audio-files/"
-
-y, sr = librosa.load('/home/ubuntu/dev/mel-spectrogram-service/data/audio-files/down/0a9f9af7_nohash_0.wav')
-
-S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, n_fft=1024)
-
-log_S = librosa.db_to_amplitude(S_db=S)
-
-plt.figure(figsize=(12,4))
-
-librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel')
-
-plt.title('mel power spectrogram')
-
-plt.colorbar(format='%+02.0f dB')
-
-plt.tight_layout()
-
-plt.savefig('/home/ubuntu/dev/mel-spectrogram-service/data/audio-files/down/0a9f9af7_nohash_0.png')
+import sys
 
 
+def generate_mel_spectrogram_from_audio_batch_files(audio_folder, mel_spectrogram_folder):
+    base_audio_path = os.path.expanduser('~/data/audio-files/') + audio_folder
+    base_mel_spectrogram_path = os.path.expanduser('~/data/mel-spectrogram-datasets/') + mel_spectrogram_folder
+    for path, folders, files in os.walk(base_audio_path):
+        for folder in folders:
+            list_of_audio_files = os.listdir(f"{path}/{folder}")
+            for audio in list_of_audio_files:
+                prefix_file_name = audio.split(".")[0]
+                audio_full_path = base_audio_path + folder + "/" + audio
+                mel_spectrogram_full_path = base_mel_spectrogram_path + "/" + folder + "/" + prefix_file_name + ".png"
+                y, sr = librosa.load(audio_full_path)
+                S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+                #mfccs = librosa.feature.mfcc(S=librosa.power_to_db(S))
+                fig, ax = plt.subplots(nrows=1, sharex=True)
+                #img = librosa.display.specshow(librosa.power_to_db(S, ref=np.max), x_axis='time', y_axis='mel',fmax=8000, ax=ax)
+                #fig.colorbar(img, ax=ax)
+                plt.savefig(mel_spectrogram_full_path)
 
 
+generate_mel_spectrogram_from_audio_batch_files("audio-files-mini-speech-command/", "mel-spectrogram-mini-speech-commands")
