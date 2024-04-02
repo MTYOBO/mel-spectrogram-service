527300f0e6890fe3474ea70420aec03032390a85
mike dlulane <mikedlulane@outlook.com>
2024-04-02 19:39:15 +0200

    WIP.

--- src/controller/wav2mel.py
+++ src/controller/wav2mel.py
@@ -24,15 +24,20 @@ def generate_mel_spectrogram_from_audio_batch_files(audio_folder, mel_spectrogra
             list_of_audio_files = os.listdir(f"{path}/{folder}")
             for audio in list_of_audio_files:
                 prefix_file_name = audio.split(".")[0]
-                audio_full_path = base_audio_path + folder + "/" + audio
+                audio_full_path = base_audio_path + "/" + folder + "/" + audio
                 mel_spectrogram_full_path = base_mel_spectrogram_path + "/" + folder + "/" + prefix_file_name + ".png"
                 y, sr = librosa.load(audio_full_path)
                 S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-                #mfccs = librosa.feature.mfcc(S=librosa.power_to_db(S))
-                fig, ax = plt.subplots(nrows=1, sharex=True)
-                #img = librosa.display.specshow(librosa.power_to_db(S, ref=np.max), x_axis='time', y_axis='mel',fmax=8000, ax=ax)
-                #fig.colorbar(img, ax=ax)
+                fig, ax = plt.subplots()
+                img = librosa.display.specshow(librosa.power_to_db(S, ref=np.max), fmax=8000, ax=ax)
+                fig.patch.set_visible(False)
+                ax.axis('off')
                 plt.savefig(mel_spectrogram_full_path)
+                plt.close()
 
 
-generate_mel_spectrogram_from_audio_batch_files("audio-files-mini-speech-command/", "mel-spectrogram-mini-speech-commands")
+# generate_mel_spectrogram_from_audio_batch_files("audio-files-gnuradio",
+#                                                 "mel-spectrogram-gnu-radio")
+
+generate_mel_spectrogram_from_audio_batch_files("audio-files-mini-speech-command",
+                                                "mel-spectrogram-mini-speech-commands")
